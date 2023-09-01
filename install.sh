c9f06edbfd89c648389099b607d7201801b36654
kevin9010 <fashuxu@163.com>
2023-09-01 16:16:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ FILE=OO_PubKey
 if test -f "$FILE"; then
   echo Patch has already been applied. Starting DocumentServer...
 else
-  apt-get update && apt-get install -y python3
+  apt-get update && apt-get install -y python3 gcc build-essential
     wget https://bootstrap.pypa.io/get-pip.py
     python3 get-pip.py
     pip3 install pycrypto
